88862ffdfa0d9d56e3d964abd3c9711283ed5fde
Aarohi Mahajan <meaarohi99@gmail.com>
2021-01-06 18:29:42 +0530

    First commit with user error

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAN_WqkFI3QcFinSiBGEhh99hXAVcP7yZU",
+    authDomain: "mygitapp-9f20c.firebaseapp.com",
+    projectId: "mygitapp-9f20c",
+    storageBucket: "mygitapp-9f20c.appspot.com",
+    messagingSenderId: "266411689136",
+    appId: "1:266411689136:web:ed166875692ba1fd4be775",
+    measurementId: "G-QNQS274416"
+  };
---
+++ src/pages/Signin.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Signin = () => {
+    return(
+        <div>
+        <h1>Sign in page</h1>
+        </div>
+    );
+};
+export default Signin;
